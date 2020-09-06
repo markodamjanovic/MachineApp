9d8dbb862871823ef8a084ca07e17d3bab7b0e84
markodamjanovic <damjanovic.m1@gmail.com>
2020-09-06 13:01:49 +0200

    template for malfunction controller. WIP

---
+++ MachineApp/Controllers/MalfunctionController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MachineApp.Models;
+
+namespace MachineApp.Controllers
+{
+    public class MalfunctionController : Controller
+    {
+        public MalfunctionController()
+        {
+            
+        }
+
+    }
+}
